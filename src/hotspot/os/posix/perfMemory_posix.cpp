a34cc6d10826fdf41910a7d36690b56a3e54cefa
Pushkar Kulkarni <pushkar.kulkarni@canonical.com>
2024-07-18 10:58:54 +0530

    Fix missing copyright headers

--- src/hotspot/os/posix/perfMemory_posix.cpp
+++ src/hotspot/os/posix/perfMemory_posix.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2001, 2021, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2012, 2021 SAP SE. All rights reserved.
+ * Copyright (c) 2020, 2021, Azul Systems, Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
